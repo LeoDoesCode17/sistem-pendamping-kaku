f694df6108e92cdcd9b8356d5375273e1b87149e
noval <novalnabihan@gmail.com>
2025-10-02 22:35:17 +0800

    refactor: rename component function from TopBar to TopBarLogin

--- src/features/ui/Topbar/TopbarLogin.tsx
+++ src/features/ui/Topbar/TopbarLogin.tsx
@@ -2,13 +2,12 @@
 
 import Time from '@/features/ui/Clock'
 
-export default function TopBar() {
+export default function TopBarLogin() {
 
-    return (
-      <div className="bg-maroon px-8 py-4 flex justify-between items-center shadow-lg">
-        <h1 className="text-cream text-3xl font-bold tracking-wide">Kaku Food</h1>
-        
-  <Time></Time>
-      </div>
-    );
-  }
+  return (
+    <div className="bg-maroon px-8 py-4 flex justify-between items-center shadow-lg">
+      <h1 className="text-cream text-3xl font-bold tracking-wide">Kaku Food</h1>
+      <Time></Time>
+    </div>
+  );
+}
