66c35dafb835f6718328a87bf32eae68b8ece62c
noval <novalnabihan@gmail.com>
2025-10-03 16:01:31 +0800

    fix(ui): fix bug double topbar appear when open order page

--- src/app/(routes)/cashier/layout.tsx
+++ src/app/(routes)/cashier/layout.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import TopbarMain from "@/features/ui/Topbar/TopbarMain";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isOrderPage = pathname.includes("/order");
+
   return (
     <div className="h-screen overflow-hidden">
-      <TopbarMain pageType="Kasir" />
-      <main>{children}</main>
+      {!isOrderPage && <TopbarMain pageType="Kasir" />}
+      <main className={isOrderPage ? "" : "h-[calc(100vh-80px)]"}>{children}</main>
     </div>
   );
 }
