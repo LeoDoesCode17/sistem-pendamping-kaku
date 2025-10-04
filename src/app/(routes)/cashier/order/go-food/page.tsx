45faf63d9e51fb0f51cea80a3c0c72f1c4e12ac4
noval <novalnabihan@gmail.com>
2025-10-05 00:40:51 +0800

    feat(ui): add gofood order dummy page

---
+++ src/app/(routes)/cashier/order/go-food/page.tsx
@@ -0,0 +1,6 @@
+import GojekorderPage from '@/features/cashier/pages/GojekorderPage';
+
+export default function Page() {
+  return <GojekorderPage/>;
+}
+
---
+++ src/features/cashier/pages/GojekorderPage.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function GojekPage() {
+  
+
+  return (
+    <div className="h-screen overflow-hidden bg-gray-100">
+      <div className="flex items-center justify-center h-full px-4">
+        <h1 className='text-maroon'>TES DUMMY GOFOOD PAGE</h1>
+      </div>
+    </div>
+  );
+}
