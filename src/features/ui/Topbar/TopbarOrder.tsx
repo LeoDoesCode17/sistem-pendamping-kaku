b7a15dc6f9c20228afd5c3db0f794c6ab1651a3a
noval <novalnabihan@gmail.com>
2025-10-05 00:45:28 +0800

    fix(ui): adjust top bar size with other topbar

--- src/features/ui/Topbar/TopbarOrder.tsx
+++ src/features/ui/Topbar/TopbarOrder.tsx
@@ -6,14 +6,14 @@ import { LogOut } from 'lucide-react';
 
 export default function TopbarOrder({ orderType }: { orderType: string }) {
   return (
-    <div className="bg-maroon px-6 py-3 flex justify-between items-center shadow-lg">
+    <div className="bg-maroon px-8 py-2 flex justify-between items-center shadow-lg">
       <button className="text-cream" aria-label="Logout">
         <LogOut />
       </button>
 
       <div className="flex flex-col items-center">
         <h1 className="text-cream text-lg sm:text-xl font-bold tracking-wide">Kaku Food</h1>
-        <div className="bg-[#992B1D] text-cream rounded-xl border-2 border-cream px-4 py-1 text-sm sm:text-lg font-bold mt-1">
+        <div className="bg-[#992B1D] text-cream rounded-xl border-2 border-cream px-4 py-px text-sm sm:text-lg font-bold">
           {orderType}
         </div>
       </div>
