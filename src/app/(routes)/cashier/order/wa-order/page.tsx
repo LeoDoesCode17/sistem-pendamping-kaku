3d4af225e639ac28b8f3269ccc19c2ead47a4cd2
noval <novalnabihan@gmail.com>
2025-10-05 00:42:04 +0800

    feat(ui): add wa order dummy page

---
+++ src/app/(routes)/cashier/order/wa-order/page.tsx
@@ -0,0 +1,6 @@
+import WaorderPage from '@/features/cashier/pages/WaorderPage';
+
+export default function Page() {
+  return <WaorderPage/>;
+}
+
--- src/features/cashier/pages/WaorderPage.tsx
+++ src/features/cashier/pages/WaorderPage.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-export default function GrabPage() {
+export default function WaorderPage() {
   
 
   return (
     <div className="h-screen overflow-hidden bg-gray-100">
       <div className="flex items-center justify-center h-full px-4">
-        <h1 className='text-maroon'>TES DUMMY GRAB PAGE</h1>
+        <h1 className='text-maroon'>TES DUMMY GOFOOD PAGE</h1>
       </div>
     </div>
   );
