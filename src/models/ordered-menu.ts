3a51043e3e762d39e30de52f1a990df358bcfd4d
Leo <leonardonifinluri@gmail.com>
2025-10-08 00:22:50 +0800

    feat: add toJson and fromJson in order menu model

--- src/models/ordered-menu.ts
+++ src/models/ordered-menu.ts
@@ -6,7 +6,8 @@ export class OrderedMenu {
   quantity: number;
   timeCreated: number | undefined;
   timeFinished: number | undefined;
-  customize: string;
+  customize: string | undefined;
+  isDone: boolean;
 
   constructor(params: {
     id: string;
@@ -14,7 +15,8 @@ export class OrderedMenu {
     quantity: number;
     timeCreated: number | undefined;
     timeFinished: number | undefined;
-    customize: string;
+    customize: string | undefined;
+    isDone: boolean;
   }) {
     this.id = params.id;
     this.menu = params.menu;
@@ -22,5 +24,42 @@ export class OrderedMenu {
     this.quantity = params.quantity;
     this.timeCreated = params.timeCreated;
     this.timeFinished = params.timeFinished;
+    this.isDone = params.isDone;
+  }
+
+  toJson(): {
+    id: string;
+    menu: string;
+    quantity: number;
+    customize: string | undefined;
+    isDone: boolean
+  } {
+    return {
+      id: this.id,
+      menu: this.menu.id,
+      quantity: this.quantity,
+      customize: this.customize,
+      isDone: false,
+    };
+  }
+
+  static fromJson(data: {
+    id: string;
+    menu: Menu;
+    quantity: number;
+    customize: string | undefined;
+    timeCreated: number | undefined;
+    timeFinished: number | undefined;
+    isDone: boolean;
+  }): OrderedMenu {
+    return new OrderedMenu({
+      id: data.id,
+      menu: data.menu,
+      quantity: data.quantity,
+      customize: data.customize,
+      timeCreated: data.timeCreated,
+      timeFinished: data.timeFinished,
+      isDone: data.isDone,
+    });
   }
 }
