366a8c70306fd4d0acbba27f062b080dd73343a8
Leo <leonardonifinluri@gmail.com>
2025-10-08 00:23:17 +0800

    feat: add fromJson and toJson in transaction model

--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -1,27 +1,66 @@
+import { TransactionCategory } from "@/types/transaction-category";
 import { OrderedMenu } from "./ordered-menu";
-import { Outlet } from "./outlet";
-import { TransactionCategory } from "./transaction-category";
 
 export class Transaction {
   id: string;
   code: string;
   category: TransactionCategory;
   orderedMenus: OrderedMenu[];
-  timeCreated: Date;
-  timeFinished: Date;
+  timeCreated: number | undefined;
+  timeFinished: number | undefined;
 
-  constructor(params: {  id: string;
-  code: string;
-  category: TransactionCategory;
-  outlet: Outlet;
-  orderedMenus: OrderedMenu[];
-  timeCreated: Date;
-  timeFinished: Date;}) {
+  constructor(params: {
+    id: string;
+    code: string;
+    category: TransactionCategory;
+    orderedMenus: OrderedMenu[];
+    timeCreated: number | undefined;
+    timeFinished: number | undefined;
+  }) {
     this.id = params.id;
     this.code = params.code;
     this.category = params.category;
     this.orderedMenus = params.orderedMenus;
     this.timeCreated = params.timeCreated;
     this.timeFinished = params.timeFinished;
+  }
+
+  static fromJson(data: {
+    id: string;
+    code: string;
+    category: string;
+    orderedMenus: OrderedMenu[];
+    timeCreated: number | undefined;
+    timeFinished: number | undefined;
+  }): Transaction {
+    return new Transaction({
+      id: data.id,
+      code: data.code,
+      category: data.category as TransactionCategory,
+      orderedMenus: data.orderedMenus,
+      timeCreated: data.timeCreated,
+      timeFinished: data.timeFinished,
+    });
+  }
+
+  toJson(): {
+    id: string;
+    code: string;
+    category: string;
+    orderedMenus: {
+      id: string;
+      menu: string;
+      quantity: number;
+      customize: string | undefined;
+    }[];
+  } {
+    return {
+      id: this.id,
+      code: this.code,
+      category: this.category,
+      orderedMenus: this.orderedMenus.map((orderedMenu) =>
+        orderedMenu.toJson()
+      ),
+    };
   }
 }
