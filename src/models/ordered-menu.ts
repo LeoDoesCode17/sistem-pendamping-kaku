96b9bf785a3a06b0ccfba459f9f570f214e9360f
Leo <leonardonifinluri@gmail.com>
2025-10-07 23:53:43 +0800

    feat: add timestamp in ordered menu

--- src/models/ordered-menu.ts
+++ src/models/ordered-menu.ts
@@ -1,22 +1,26 @@
 import { Menu } from "./menu";
-import { Outlet } from "./outlet";
 
 export class OrderedMenu {
   id: string;
   menu: Menu;
   quantity: number;
+  timeCreated: number | undefined;
+  timeFinished: number | undefined;
   customize: string;
 
   constructor(params: {
     id: string;
     menu: Menu;
     quantity: number;
-    outlet: Outlet;
+    timeCreated: number | undefined;
+    timeFinished: number | undefined;
     customize: string;
   }) {
     this.id = params.id;
     this.menu = params.menu;
     this.customize = params.customize;
     this.quantity = params.quantity;
+    this.timeCreated = params.timeCreated;
+    this.timeFinished = params.timeFinished;
   }
 }
