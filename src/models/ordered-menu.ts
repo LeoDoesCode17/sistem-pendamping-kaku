4ada43aec1944d5a8d658ca017ae40dd71ae1ca6
Leo <leonardonifinluri@gmail.com>
2025-10-07 23:02:02 +0800

    refactor: remove timestamp field from ordered menu model

--- src/models/ordered-menu.ts
+++ src/models/ordered-menu.ts
@@ -5,23 +5,17 @@ export class OrderedMenu {
   id: string;
   menu: Menu;
   quantity: number;
-  timeCreated: Date;
-  timeFinished: Date;
   customize: string;
 
   constructor(params: {
     id: string;
     menu: Menu;
     quantity: number;
-    timeCreated: Date;
-    timeFinished: Date;
     outlet: Outlet;
     customize: string;
   }) {
     this.id = params.id;
     this.menu = params.menu;
-    this.timeCreated = params.timeCreated;
-    this.timeFinished = params.timeFinished;
     this.customize = params.customize;
     this.quantity = params.quantity;
   }
