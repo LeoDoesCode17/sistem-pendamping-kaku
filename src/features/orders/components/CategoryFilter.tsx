cf69d4f389b0eb9a385aed2802cba3553f374d34
Leo <leonardonifinluri@gmail.com>
2025-10-16 14:42:07 +0800

    feat: show category based on MenuCategory enum

--- src/features/orders/components/CategoryFilter.tsx
+++ src/features/orders/components/CategoryFilter.tsx
@@ -1,34 +1,50 @@
 // features/orders/components/CategoryFilter.tsx
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { CATEGORIES, CategoryId } from '../constants/categories';
+import { useState } from "react";
+import { CategoryId } from "../constants/categories";
+import { MenuCategory } from "@/types/menu-category";
 
 interface CategoryFilterProps {
   onCategoryChange?: (category: CategoryId) => void;
+  myOnCategoryChange?: (category: MenuCategory | string) => void;
 }
 
-export default function CategoryFilter({ onCategoryChange }: CategoryFilterProps) {
-  const [activeCategory, setActiveCategory] = useState<CategoryId>('all');
+export default function CategoryFilter({
+  myOnCategoryChange
+}: CategoryFilterProps) {
 
-  const handleCategoryClick = (categoryId: CategoryId) => {
-    setActiveCategory(categoryId);
-    onCategoryChange?.(categoryId);
+  const [myActiveCategory, setMyActiveCategory] = useState<string>("all");
+
+  const myHandleCategoryClick = (category: MenuCategory | string) => {
+    setMyActiveCategory(category);
+    myOnCategoryChange?.(category);
   };
 
   return (
     <div className="flex gap-3 mb-6 flex-wrap">
-      {CATEGORIES.map((category) => (
-        <button
-          key={category.id}
-          onClick={() => handleCategoryClick(category.id)}
+              <button
+          key="all"
+          onClick={() => myHandleCategoryClick("all")}
           className={`px-6 py-3 rounded-lg font-semibold text-base transition-colors border-2 border-black  ${
-            activeCategory === category.id
-              ? 'bg-maroon text-cream'
-              : 'bg-cream text-maroon'
+            myActiveCategory === "all"
+              ? "bg-maroon text-cream"
+              : "bg-cream text-maroon"
           }`}
         >
-          {category.label}
+          ALL
+        </button>
+      {Object.values(MenuCategory).map((menuCategory) => (
+        <button
+          key={menuCategory}
+          onClick={() => myHandleCategoryClick(menuCategory)}
+          className={`px-6 py-3 rounded-lg font-semibold text-base transition-colors border-2 border-black  ${
+            myActiveCategory === menuCategory
+              ? "bg-maroon text-cream"
+              : "bg-cream text-maroon"
+          }`}
+        >
+          {menuCategory}
         </button>
       ))}
     </div>
