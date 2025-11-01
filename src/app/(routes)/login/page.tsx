573a63ed51faaf4c5b59886d9ea3830599af0767
noval <novalnabihan@gmail.com>
2025-11-02 00:51:47 +0800

    fix: fix error while try to deploy

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,5 +1,18 @@
-import LoginPage from '@/features/auth/pages/LoginPage';
+// src/app/(routes)/login/page.tsx
+import { Suspense } from 'react';
+import LoginForm from '@/features/auth/components/LoginForm';
 
-export default function Page() {
-  return <LoginPage />;
+// Opsi tambahan untuk mencegah prerender error saat build
+export const dynamic = 'force-dynamic';
+
+export default function LoginPage() {
+  return (
+    <div className="h-screen overflow-hidden bg-gray-100">
+      <div className="flex h-full items-center justify-center px-4">
+        <Suspense fallback={<div />}>
+          <LoginForm />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
