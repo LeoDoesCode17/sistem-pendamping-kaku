0360ba773109c928c3c8089e8b5bcc0e62091146
noval <novalnabihan@gmail.com>
2025-12-06 13:12:39 +0800

    feat: adjust topbar

--- src/app/(routes)/packager/layout.tsx
+++ src/app/(routes)/packager/layout.tsx
@@ -2,9 +2,9 @@ import TopbarMain from "@/features/ui/Topbar/TopbarMain";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 h-screen overflow-hidden">
       <TopbarMain pageType="Packager" />
-      <main>{children}</main>
+      <main className="pt-20 h-screen overflow-y-auto overscroll-contain">{children}</main>
     </div>
   );
 }
