c72349937207fe22d43d9207ce5adbd7383e92df
noval <novalnabihan@gmail.com>
2025-12-06 15:15:25 +0800

    fix: adjust menu category

--- src/features/orders/constants/categories.ts
+++ src/features/orders/constants/categories.ts
@@ -3,10 +3,11 @@ export const CATEGORIES = [
   { id: 'all', label: 'All' },
   { id: 'gorengan', label: 'Gorengan' },
   { id: 'rebusan', label: 'Rebusan' },
-  { id: 'cimol', label: 'Cimol' },
+  { id: 'cireng', label: 'Cireng' },
   { id: 'pisang', label: 'Pisang' },
   { id: 'minuman', label: 'Minuman' },
-  { id: 'sambal', label: 'Sambal' }
+  { id: 'sambal', label: 'Sambal' },
+  { id: 'setengahmatang', label: 'Setengah Matang' },
 ] as const;
 
 export type CategoryId = typeof CATEGORIES[number]['id'];
